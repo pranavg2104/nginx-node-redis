c76cdb711c8f0974f5575512574750f0798b85cb
Pranav Govekar <pranavg2104@gmail.com>
2026-05-13 22:18:35 +0530

    updating the CIDR blocks

--- terra-config/main.tf
+++ terra-config/main.tf
@@ -25,14 +25,14 @@ resource "aws_security_group" "redis_security_group" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "SSH access"
   }
   ingress{
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
     description = "HTTP access"
   }
 
