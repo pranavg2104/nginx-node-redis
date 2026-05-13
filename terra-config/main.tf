ff2fdac128d424407dd15088269255d58230f203
Pranav Govekar <pranavg2104@gmail.com>
2026-05-13 22:47:22 +0530

    Added Egress to the SG

--- terra-config/main.tf
+++ terra-config/main.tf
@@ -36,6 +36,14 @@ resource "aws_security_group" "redis_security_group" {
     description = "HTTP access"
   }
 
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "Internet access"
+  }
+
   tags = {
     Name = "${var.app_name}-sg"
   }
